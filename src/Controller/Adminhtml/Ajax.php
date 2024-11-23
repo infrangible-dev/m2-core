004940b76f9ac51e997e978eee0bd56123b5fc70
Andreas Knollmann <a.knollmann@seak.dev>
2024-11-23 17:44:56 +0100

    split Ajax/Json controller

--- src/Controller/Adminhtml/Ajax.php
+++ src/Controller/Adminhtml/Ajax.php
@@ -4,176 +4,53 @@ declare(strict_types=1);
 
 namespace Infrangible\Core\Controller\Adminhtml;
 
-use Exception;
-use FeWeDev\Base\Arrays;
-use FeWeDev\Base\Json;
-use Magento\Framework\App\Action\Action;
-use Magento\Framework\App\Action\Context;
-use Magento\Framework\App\Request\Http;
-use Magento\Framework\App\RequestInterface;
-use Magento\Framework\App\ResponseInterface;
-use Magento\Framework\Exception\NotFoundException;
-use Psr\Log\LoggerInterface;
+use Magento\Framework\App\Response\HttpInterface;
 
 /**
  * @author      Andreas Knollmann
  * @copyright   Copyright (c) 2014-2024 Softwareentwicklung Andreas Knollmann
  * @license     http://www.opensource.org/licenses/mit-license.php MIT
  */
-abstract class Ajax
-    extends Action
+abstract class Ajax extends Json
 {
-    /** @var Arrays */
-    protected $arrays;
-
-    /** @var Json */
-    protected $json;
-
-    /** @var LoggerInterface */
-    protected $logging;
-
-    /** @var int */
-    private $responseCode = 200;
-
     /** @var bool */
     private $responseResult = true;
 
-    /** @var array */
-    private $responseValues = [];
-
-    /**
-     * @param Arrays $arrays
-     * @param Json $json
-     * @param Context $context
-     * @param LoggerInterface $logging
-     */
-    public function __construct(
-        Arrays $arrays,
-        Json $json,
-        Context $context,
-        LoggerInterface $logging
-    ) {
-        parent::__construct($context);
-
-        $this->arrays = $arrays;
-        $this->json = $json;
-
-        $this->logging = $logging;
-    }
-
-    /**
-     * @param string $key
-     * @param mixed $value
-     */
-    protected function addResponseValue(string $key, $value)
-    {
-        $keys = explode(':', $key);
-
-        $this->responseValues = $this->arrays->addDeepValue($this->responseValues, $keys, $value);
-    }
-
-    /**
-     * @param array $values
-     */
-    protected function addResponseValues(array $values)
-    {
-        foreach ($values as $key => $value) {
-            $this->addResponseValue($key, $value);
-        }
-    }
-
-    /**
-     * @param string $key
-     *
-     * @return mixed
-     */
-    protected function getResponseValue(string $key)
-    {
-        if (isset ($this->responseValues[$key])) {
-            return $this->responseValues[$key];
-        }
-
-        return null;
-    }
-
-    /**
-     * Reset response values
-     */
-    protected function resetResponseValues()
-    {
-        $this->responseValues = [];
-    }
-
-    /**
-     * @param int $responseCode
-     */
-    public function setResponseCode(int $responseCode)
-    {
-        $this->responseCode = $responseCode;
-    }
-
-    /**
-     * @param string $message
-     */
-    protected function setSuccessResponse(string $message)
+    protected function setSuccessResponse(string $message): void
     {
         $this->responseResult = true;
-        $this->responseValues = ['message' => $message];
+        $this->addResponseValue(
+            'message',
+            $message
+        );
     }
 
-    /**
-     * @param string $message
-     */
-    protected function setErrorResponse(string $message)
+    protected function setErrorResponse(string $message): void
     {
         $this->responseResult = false;
-        $this->responseValues = ['message' => $message];
+        $this->addResponseValue(
+            'message',
+            $message
+        );
     }
 
-    /**
-     * @param string $message
-     */
-    protected function setFatalResponse(string $message)
+    protected function setFatalResponse(string $message): void
     {
         $this->setErrorResponse($message);
         $this->setResponseCode(500);
     }
 
-    /**
-     * Dispatch request
-     *
-     * @param RequestInterface $request
-     *
-     * @return ResponseInterface
-     * @throws NotFoundException
-     */
-    public function dispatch(RequestInterface $request)
+    protected function handleException(\Exception $exception): void
     {
-        /** @var Http $request */
-        $request = $this->getRequest();
+        $this->logging->error($exception);
+        $this->setErrorResponse($exception->getMessage());
+    }
 
-        if ($request->isAjax()) {
-            try {
-                parent::dispatch($request);
-            } catch (Exception $exception) {
-                $this->logging->error($exception);
-                $this->setErrorResponse($exception->getMessage());
-            }
+    protected function processResponse(array $responseData): HttpInterface
+    {
+        $responseData = $this->arrays->mergeArrays(['success' => $this->responseResult],
+            $responseData);
 
-            $responseData =
-                $this->arrays->mergeArrays(['success' => $this->responseResult], $this->responseValues);
-
-            /** @var \Magento\Framework\App\Response\Http $response */
-            $response = $this->getResponse();
-
-            $response->setHttpResponseCode($this->responseCode);
-            $response->setHeader('Content-type', 'application/json');
-
-            $response->setBody($this->json->encode($responseData));
-
-            return $response;
-        } else {
-            return parent::dispatch($request);
-        }
+        return parent::processResponse($responseData);
     }
 }
